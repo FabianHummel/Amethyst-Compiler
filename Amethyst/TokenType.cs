5a92fff037b328727632f685c7520b9f98c922bd
Fabian Hummel <hum22501@spengergasse.at>
2024-03-17 22:13:18 +0100

    Removed unused token description

--- Amethyst/TokenType.cs
+++ Amethyst/TokenType.cs
@@ -1,112 +1,62 @@
-using System.ComponentModel;
-
 namespace Amethyst;
 
 public enum TokenType
 {
     // Single-character tokens.
-    [Description("(")]
     LEFT_PAREN,
-    [Description(")")]
     RIGHT_PAREN,
-    [Description("{")]
     LEFT_BRACE,
-    [Description("}")]
     RIGHT_BRACE,
-    [Description("[")]
     LEFT_BRACKET,
-    [Description("]")]
     RIGHT_BRACKET,
-    [Description(",")]
     COMMA,
-    [Description(".")]
     DOT,
-    [Description("-")]
     MINUS,
-    [Description("+")]
     PLUS,
-    [Description(";")]
     SEMICOLON,
-    [Description(":")]
     COLON,
-    [Description("/")]
     SLASH,
-    [Description("*")]
     STAR,
-    [Description("%")]
     MODULO,
 
     // One or two character tokens.
-    [Description("!")]
     BANG,
-    [Description("!=")]
     BANG_EQUAL,
-    [Description("=")]
     EQUAL,
-    [Description("==")]
     EQUAL_EQUAL,
-    [Description(">")]
     GREATER,
-    [Description(">=")]
     GREATER_EQUAL,
-    [Description("<")]
     LESS,
-    [Description("<=")]
     LESS_EQUAL,
-    [Description("+=")]
     PLUS_EQUAL,
-    [Description("-=")]
     MINUS_EQUAL,
-    [Description("/=")]
     SLASH_EQUAL,
-    [Description("*=")]
     STAR_EQUAL,
-    [Description("%=")]
     MODULO_EQUAL,
 
     // Literals.
-    [Description("Identifier")]
     IDENTIFIER, 
-    [Description("String")]
     STRING,
-    [Description("Number")]
     NUMBER,
 
     // Keywords.
-    [Description("and")]
     AND,
-    [Description("else")]
     ELSE,
-    [Description("false")]
     FALSE,
-    [Description("function")]
     FUNCTION,
-    [Description("ticking")]
     TICKING,
-    [Description("initializing")]
     INITIALIZING,
-    [Description("namespace")]
     NAMESPACE,
-    [Description("for")]
     FOR,
-    [Description("if")]
     IF,
-    [Description("null")]
     NULL,
-    [Description("or")]
     OR,
-    [Description("out")]
     OUT,
-    [Description("true")]
     TRUE, 
-    [Description("var")]
     VAR, 
-    [Description("while")]
     WHILE,
-    [Description("print")]
     PRINT,
     
     // File.
-    [Description("end of file")]
     EOF,
 }
