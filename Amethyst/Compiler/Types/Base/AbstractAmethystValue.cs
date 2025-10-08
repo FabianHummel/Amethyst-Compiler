a829ccf24df972c509364a28172330f3dcc00d0e
Fabian Hummel <98157550+FabianHummel@users.noreply.github.com>
2025-10-08 09:46:57 +0200

    Merge type specific classes

--- Amethyst/Compiler/Types/Base/AbstractAmethystValue.cs
+++ Amethyst/Compiler/Types/Base/AbstractAmethystValue.cs
@@ -4,6 +4,7 @@ public abstract class AbstractAmethystValue : AbstractValue
 {
     protected override AbstractValue VisitAdd(AbstractValue rhs)
     {
+        // TODO: I don't like this being here. Find a way to move this to the specific type (e.g. AbstractString)
         if (rhs is AbstractString abstractString)
         {
             return AbstractString.VisitAdd(this, abstractString);
